07e075cdf627daceb372051d37ba0a1b67795533
Peter Atef <peter.atef@scandiweb.com>
2024-08-01 17:01:15 +0300

    Update index to export all ui elements

--- packages/ui/index.ts
+++ packages/ui/index.ts
@@ -1,67 +1,46 @@
-export {
-  ChartContainer,
-  ChartLegend,
-  ChartLegendContent,
-  ChartStyle,
-  ChartTooltip,
-  ChartTooltipContent,
-} from './components/chart';
-export type { ChartConfig } from './components/chart';
-export { Button } from './components/button';
-export { Sheet, SheetContent, SheetTrigger } from './components/sheet';
-export { Input } from './components/input';
-export {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuPortal,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
-  DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
-  DropdownMenuTrigger,
-} from './components/dropdown-menu';
-export {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-  CardFooter,
-} from './components/card';
-export { Badge } from './components/badge';
-export { Avatar, AvatarImage, AvatarFallback } from './components/avatar';
-export {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectScrollDownButton,
-  SelectScrollUpButton,
-  SelectSeparator,
-  SelectTrigger,
-  SelectValue,
-} from './components/select';
-export { Separator } from './components/separator';
-export {
-  Carousel,
-  CarouselContent,
-  CarouselDots,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from './components/carousel';
-export { Switch } from './components/switch';
-export { Label } from './components/label';
+export * from './components/accordion';
+export * from './components/alert-dialog';
+export * from './components/alert';
+export * from './components/aspect-ratio';
+export * from './components/avatar';
+export * from './components/badge';
+export * from './components/breadcrumb';
+export * from './components/button';
+export * from './components/card';
+export * from './components/carousel';
+export * from './components/chart';
+export * from './components/checkbox';
+export * from './components/collapsible';
+export * from './components/command';
+export * from './components/context-menu';
+export * from './components/dialog';
+export * from './components/drawer';
+export * from './components/dropdown-menu';
+export * from './components/form';
+export * from './components/hover-card';
+export * from './components/input-otp';
+export * from './components/input';
+export * from './components/label';
+export * from './components/menubar';
+export * from './components/navigation-menu';
 export * from './components/pagination';
+export * from './components/popover';
+export * from './components/progress';
+export * from './components/radio-group';
+export * from './components/resizable';
+export * from './components/scroll-area';
+export * from './components/select';
+export * from './components/separator';
+export * from './components/sheet';
+export * from './components/skeleton';
+export * from './components/slider';
+export * from './components/switch';
+export * from './components/table';
+export * from './components/tabs';
 export * from './components/textarea';
-export * from './components/toast';
-export * from './components/use-toast';
 export * from './components/toaster';
+export * from './components/toast';
+export * from './components/toggle-group';
+export * from './components/toggle';
+export * from './components/tooltip';
+export * from './components/use-toast';
